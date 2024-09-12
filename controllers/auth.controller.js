f3533cb72fb1199cb323f42f7b30b84bfd749232
Onasanya Daniel <91058251+Onas-2000@users.noreply.github.com>
2024-09-13 01:23:04 +0200

    Update auth.controller.js
    imported jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+
 
 const registerUserController = async (req, res) => {
   const { email, password } = req.body;
